213eac78cfe88a119ed805146e50926f70d21a22
Sidharth S Nair <sidharthsnair357@gmail.com>
2026-04-15 06:23:45 +0530

    Add files via upload

---
+++ 21 Days/D6.py
@@ -0,0 +1,6 @@
+import random
+print("Random number and alphabet generator")
+list_a= ["a", "b", "c", "d", "e"]
+list_n= ["1", "2", "3", "4", "5"]
+print("\nAlphabet:", random.choice(list_a))
+print("Number:", random.choice(list_n))
