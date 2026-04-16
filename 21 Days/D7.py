40f3cfdb376ef44876f822266e5d03eb14dabe14
Sidharth S Nair <sidharthsnair357@gmail.com>
2026-04-16 12:37:24 +0530

    Add files via upload

---
+++ 21 Days/D7.py
@@ -0,0 +1,59 @@
+import random
+
+while True:
+    s = random.randint(1,6)
+    
+    if s == 1:
+        print("⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬛⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜")
+        print("Would you like to throw the 🎲 again? (y / n)")
+        choice = input()
+        if choice == "n":
+            print("Thanks for playing with us! See you next time!")
+            break
+        else:
+            print("")
+    elif s == 2:
+        print("⬜⬜⬜⬜⬜\n⬜⬛⬜⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬛⬜\n⬜⬜⬜⬜⬜")
+        print("Would you like to throw the 🎲 again? (y / n)")
+        choice = input()
+        if choice == "n":
+            print("Thanks for playing with us! See you next time!")
+            break
+        else:
+            print("")
+    elif s == 3:
+        print("⬜⬜⬜⬜⬜\n⬜⬛⬜⬜⬜\n⬜⬜⬛⬜⬜\n⬜⬜⬜⬛⬜\n⬜⬜⬜⬜⬜")
+        print("Would you like to throw the 🎲 again? (y / n)")
+        choice = input()
+        if choice == "n":
+            print("Thanks for playing with us! See you next time!")
+            break
+        else:
+            print("")
+    elif s == 4:
+        print("⬜⬜⬜⬜⬜\n⬜⬛⬜⬛⬜\n⬜⬜⬜⬜⬜\n⬜⬛⬜⬛⬜\n⬜⬜⬜⬜⬜")
+        print("Would you like to throw the 🎲 again? (y / n)")
+        choice = input()
+        if choice == "n":
+            print("Thanks for playing with us! See you next time!")
+            break
+        else:
+            print("")
+    elif s == 5:
+        print("⬜⬜⬜⬜⬜\n⬜⬛⬜⬛⬜\n⬜⬜⬛⬜⬜\n⬜⬛⬜⬛⬜\n⬜⬜⬜⬜⬜")
+        print("Would you like to throw the 🎲 again? (y / n)")
+        choice = input()
+        if choice == "n":
+            print("Thanks for playing with us! See you next time!")
+            break
+        else:
+            print("")
+    elif s == 6:
+        print("⬜⬜⬜⬜⬜\n⬜⬛⬜⬛⬜\n⬜⬛⬜⬛⬜\n⬜⬛⬜⬛⬜\n⬜⬜⬜⬜⬜")
+        print("Would you like to throw the 🎲 again? (y / n)")
+        choice = input()
+        if choice == "n":
+            print("Thanks for playing with us! See you next time!")
+            break
+        else:
+            print("")
