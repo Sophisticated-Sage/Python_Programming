2d27dff0b4b3b3590ee3be582f41ce0d2c2b226c
Sidharth S Nair <sidharthsnair357@gmail.com>
2026-04-27 22:10:03 +0530

    Fixing errors in the code

--- 21 Days/D17.py
+++ 21 Days/D17.py
@@ -1,1 +1,14 @@
+email = input("Enter your email: ")
 
+if "@" in email:
+    username, domain = email.split("@")
+    
+    if "." in domain:
+        domain_name, extension = domain.split(".", 1)
+        print("Username:", username)
+        print("Domain:", domain_name)
+        print("Extension:", extension)
+    else:
+        print("Invalid email format (missing extension)")
+else:
+    print("Invalid email format (missing @)")
